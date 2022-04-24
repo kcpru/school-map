3fd4547d3a549e06a981cfbce3e5660a8028fdb3
bad <badatnames@tutanota.com>
2022-04-24 14:11:02 +0200

    Navigation refactoring

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -6,53 +6,39 @@
 // I HATE DYNAMIC TYPING
 
 class NavPoint {
-  constructor(point_data) {
-    const attrs = point_data["$"];
-    this.x = parseInt(attrs["cx"]);
-    this.y = parseInt(attrs["cy"]);
-    this.id = attrs["id"];
-    this.poi_id = attrs["navpoint_poi_id"] ?? undefined;
-
-    // Using an array here, since we don't have a lot of points so it's much faster, cause cpu caches
-    this.neighboor_ids = new Set();
-    for (const neighboor of point_data["nav:neighboor"]) {
-      this.neighboor_ids.add(neighboor["$"]["neighboor_id"]);
-    }
-  }
-
-  load_neighboors(nav) {
+  constructor(id, x, y, poi_id = undefined) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.poi_id = poi_id;
     this.neighboors = [];
-    for (const id of this.neighboor_ids.keys()) {
-      const neighboor = nav.nav_points.get(id);
+  }
+
+  // Add a neighboor ensuring there are no duplicates
+  add_neighboor(neighboor) {
+    if (!this.neighboors.some((n) => n.id == neighboor.id)) {
       this.neighboors.push(neighboor);
-      neighboor.ensure_neighboor(this);
-    }
-    this.sort_neighboors();
-  }
-
-  sort_neighboors() {
-    this.neighboors.sort(
-      (a, b) => this.distance_squared(a) < this.distance_squared(b)
-    );
-  }
-
-  ensure_neighboor(neighboor) {
-    if (this.neighboors == undefined) {
-      this.neighboor_ids.add(neighboor.id);
-    } else {
-      if (!this.neighboors.some((n) => n.id == neighboor.id)) {
-        this.neighboors.push(neighboor);
-        this.sort_neighboors();
-      }
     }
   }
 
   distance_squared(to) {
     return Math.pow(this.x - to.x, 2) + Math.pow(this.y - to.y);
   }
+
+  static fromSVGData(point) {
+      const attrs = point["$"];
+      const x = parseInt(attrs["cx"]);
+      const y = parseInt(attrs["cy"]);
+      const id = attrs["id"];
+      const poi_id = attrs["navpoint_poi_id"] ?? undefined;
+
+      return new NavPoint(id, x, y, poi_id);
+  }
 }
 
 class Path {
+  // You could lower memory consumption by using a reference to a previous point on the path instead of cloning the points
+  // Again doesn't matter here
   constructor() {
     this.points = [];
   }
@@ -62,6 +48,9 @@ class Path {
     new_path.points = [...this.points];
     return new_path;
   }
+
+  // Performance note: probably the easiest way to speed this up would be to cache the result of this function.
+  // It's not neccesarry for this use case but if anyone needs to speed this up this is the place to do so
   get_distance_squared() {
     let distance = 0;
     for (let i = 1; i < this.points.length; i++) {
@@ -72,19 +61,29 @@ class Path {
 }
 
 class Nav {
-  constructor(nav_data) {
+  /**
+ * @param {NavPoint[]} points 
+ * @param {[string,string][]} vertecies - A collection of tuples of pairs of point ids
+ */
+  constructor(points, vertecies) {
     this.nav_points = new Map();
-    for (const point of nav_data) {
-      const nav_point = new NavPoint(point);
+    this.poi_nav_point = new Map();
 
-      this.nav_points.set(nav_point.id, nav_point);
+    for(const point of points) {
+      this.nav_points.set(point.id, point);
+      if(point.poi_id) {
+        this.poi_nav_point.set(point.poi_id, point)
+      }
     }
-    for (const nav_point of this.nav_points.values()) {
-      nav_point.load_neighboors(this);
+    for(const vertecie of vertecies) {
+      let left = this.nav_points.get(vertecie[0]);      
+      let right = this.nav_points.get(vertecie[1]);
+      left.add_neighboor(right)
+      right.add_neighboor(left)
     }
   }
 
-  // A very unoptimized search but good enough for our use case
+  // A very unoptimized search but good enough for our use case Replace with dijkstra for speed
   shortest_path(from, to, searched = new Map()) {
     const paths = [];
     searched.set(from, null);
@@ -119,6 +118,19 @@ class Nav {
       searched.set(from, sh_path);
       return sh_path;
     }
+  }
+  static fromSVGData(nav_data) {
+    const points = []
+    const vertecies = []
+    for (const point_data of nav_data) {
+      let point = NavPoint.fromSVGData(point_data)
+      points.push(point)
+      for (const neighboor of point_data["nav:neighboor"]) {
+        let right = neighboor["$"]["neighboor_id"];
+        vertecies.push([point.id,right])
+      }
+    }
+    return new Nav(points, vertecies)
   }
 }
 
--- src/nav/test_nav.js
+++ src/nav/test_nav.js
@@ -1,7 +1,7 @@
 const data = require("../assets/maps/pietro1.json");
 const nav = require("./nav");
 
-let nav_obj = new nav.Nav(data["svg"]["circle"]);
+let nav_obj = nav.Nav.fromSVGData(data["svg"]["circle"]);
 let from = nav_obj.nav_points.get("navpoint_27");
 let to = nav_obj.nav_points.get("navpoint_K");
 console.log(from.id, to.id);
